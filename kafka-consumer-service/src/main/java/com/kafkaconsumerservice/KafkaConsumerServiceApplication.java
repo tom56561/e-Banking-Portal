7d8051c4750df363a3c5962ced1b31b15db10f9d
tom56561 <tom56561@gmail.com>
2023-03-07 17:08:12 -0500

    init kafka service

---
+++ kafka-consumer-service/src/main/java/com/kafkaconsumerservice/KafkaConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaconsumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerServiceApplication.class, args);
+	}
+
+}
