fa1a891f859d60cb6c6fbf03a2f3e802d05ee010
tom56561 <tom56561@gmail.com>
2023-03-09 16:15:48 -0500

    insert OpenAPI setting

--- kafka-consumer-service/src/main/java/com/kafkaconsumerservice/KafkaConsumerServiceApplication.java
+++ kafka-consumer-service/src/main/java/com/kafkaconsumerservice/KafkaConsumerServiceApplication.java
@@ -1,9 +1,12 @@
 package com.kafkaconsumerservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Product APU", version = "2.0", description = "Product Microservice"))
 public class KafkaConsumerServiceApplication {
 
 	public static void main(String[] args) {
