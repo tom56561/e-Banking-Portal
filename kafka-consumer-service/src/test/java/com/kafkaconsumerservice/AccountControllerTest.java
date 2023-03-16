c4525bad7d302d12f9f9cd9b7ffdff0639257b06
tom56561 <tom56561@gmail.com>
2023-03-16 17:19:01 -0400

    add AccountControllerTest

--- kafka-consumer-service/src/test/java/com/kafkaconsumerservice/AccountControllerTest.java
+++ kafka-consumer-service/src/test/java/com/kafkaconsumerservice/AccountControllerTest.java
@@ -4,11 +4,49 @@ import com.kafkaconsumerservice.config.SecurityConfig;
 import com.kafkaconsumerservice.controller.AccountController;
 import com.kafkaconsumerservice.controller.AuthController;
 import com.kafkaconsumerservice.service.TokenService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest({AccountController.class, AuthController.class})
 @Import({SecurityConfig.class, TokenService.class})
-public class AuthConrollerTest {
-    
+public class AccountControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void rootWhenUnauthenticatedThen401() throws Exception {
+        this.mvc.perform(get("/"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void rootWhenAuthenticatedThenSaysHelloUser() throws Exception {
+        MvcResult result = this.mvc.perform(post("/auth/token")
+                        .with(httpBasic("eddie", "1234")))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String token = result.getResponse().getContentAsString();
+
+        this.mvc.perform(get("/")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(content().string("Hello, eddie"));
+    }
+
+    @Test
+    @WithMockUser
+    public void rootWithMockUserStatusIsOK() throws Exception {
+        this.mvc.perform(get("/")).andExpect(status().isOk());
+    }
 }
